e6cf1c75e0451b262558bff2e8c4ba3148642046
zeyli0 <zeyli0@163.com>
2022-08-24 23:38:08 +0800

    商品分类

--- pages/goods_cate.vue
+++ pages/goods_cate.vue
@@ -2,23 +2,48 @@
 	<view class="productSort">
 		<search ref="search" v-model="inputVal" @confirm="confirm" placeholder="点击搜索商品信息"/>
 		<view class="aside" :style="`height:${asideHeight}px;bottom:0px;`">
-			<scroll-view scroll-y="true" >
+			<scroll-view scroll-y="true">
 				<view v-for="(item, index) in category"
 				:key="item.name+index"
 				:class="['item', currentCateIdx === index ? 'active': '']" 
-				@click="handleActive">
+				@click="handleActive(index)">
 					<text>{{item.name}}</text>
 				</view>
 			</scroll-view>
 		</view>
 		<view class="content">
-			<scroll-view scroll-y="true" :style="`height: ${asideHeight}px`">
+			<view class="box"></view>
+			<scroll-view scroll-y="true" 
+			:scroll-into-view="toView"
+			scroll-with-animation="true"
+			:style="`height: ${asideHeight}px`">
 				<view class="listw"
-				v-for="(itm, idx) in lists"
+				v-for="(itm, idx) in category"
 				:key="itm.name"
-				id="'b'+idx">
-					<text>{{itm.name}}</text>
+				:id="'b'+idx">
+					<view class="title">
+						<view class="line"></view>
+						<view class="name">
+							{{itm.name}}
+						</view>
+						<view class="line"></view>
+					</view>
+					<view class="list">
+						<navigator v-for="(x, i) in itm.child"
+						:key="x.id"
+						url="navigate/navigate" 
+						class="item">
+							<view class="picture skeleton-rect"
+							style="background-color: rgb(247, 247, 247)">
+								<image :src="x.extra" mode="aspectFill"></image>
+							</view>
+							<view class="name line1">
+								{{x.name}}
+							</view>
+						</navigator>
+					</view>
 				</view>
+				<view style="height: 418px;"></view>
 			</scroll-view>
 		</view>
 	</view>
@@ -33,7 +58,9 @@ export default {
 			asideHeight: 0,
 			currentCateIdx: 0,
 			category: [],
-			lists: []
+			toView: "",
+			animation: null,
+			animationData: {}
 		} 
 	},
 	onLoad() {
@@ -43,7 +70,13 @@ export default {
 				_this.asideHeight = res.screenHeight - 54 - 50
 			}
 		})
-	} ,
+	},
+	onShow() {
+		this.animation = uni.createAnimation({
+			duration:1000,
+			timingFunction:'ease'
+		})
+	},
 	mounted() {
 		this.getCategory()
 		const query = uni.createSelectorQuery().in(this)
@@ -58,8 +91,14 @@ export default {
 		
 	},
 	methods: {
-		handleActive() {
-			
+		handleActive(index) {
+			this.currentCateIdx = index
+			this.toView = 'b' + index
+			console.log(this.toView)
+		},
+		rotateAndScal() {
+			this.animation.rotate(45).scale(2, 2).step()
+			this.animationData = this.animation.export()
 		},
 		confirm(e) {
 			if(!e) {
@@ -128,6 +167,55 @@ export default {
 		margin-left: 90px;
 		padding: 0 7px;
 		background-color: #fff;
+		.listw {
+			padding-top: 10px;
+			.title {
+				height: 45px;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				.line {
+					width: 50px;
+					height: 1px;
+					background-color: #f0f0f0;
+				}
+				.name {
+					font-size: 14px;
+					color: #333;
+					margin: 0 15px;
+					font-weight: 700;
+				}
+			}
+			.list {
+				display: flex;
+				flex-wrap: wrap;
+				.item {
+					width: 88px;
+					margin-top: 13px;
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					.picture {
+						width: 60px;
+						height: 60px;
+						border-radius: 50%;
+						uni-image {
+							width: 100%;
+							height: 100%;
+							border-radius: 50%;
+						}
+					}
+					.name {
+						font-size: 12px;
+						color: #333;
+						height: 28px;
+						line-height: 28px;
+						width: 60px;
+						text-align: center;
+					}
+				}
+			}
+		}
 	}
 }
 </style>
